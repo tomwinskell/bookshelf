c2576877b7b7abaa0b655b3501b967712be39a54
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2025-02-05 13:49:13 -0500

    Added infinite scrolling, loading further pages of the API with fetch.

--- main.js
+++ main.js
@@ -1,27 +1,40 @@
+const loader = document.getElementById("loader");
+
 document.querySelector('.search').addEventListener('click', (e) => {
-  (async() => {
-    renderBooks(await fetchJson(getInput(e.target)));
-  })();
+  getInput(e.target);
+  fetchBooks(query);
 });
+
+let startIndex = 0;
+let query;
+let isFetching = false;
+let hasMoreData = true;
 
 function getInput(target) {
   const el = document.getElementById('search-query');
-  return el.value;
+  query = el.value;
 }
 
-async function fetchJson(value) {
-  const valArr = value.split(' ');
+async function fetchBooks() {
+  if ( isFetching || !hasMoreData ) return;
+  isFetching = true;
+  loader.style.display = 'block';
+  
   const res = await fetch(
-    `https://www.googleapis.com/books/v1/volumes?q=${valArr.reduce(
-      (struct, current, currentIndex) => {
-        currentIndex === 0 ? (struct += current) : (struct += `+${current}`);
-        return struct;
-      },
-      ''
-    )}`
+    `https://www.googleapis.com/books/v1/volumes?q=${query}&startIndex=${startIndex}`
   );
   const data = await res.json();
-  return data.items;
+
+  if ( data.length === 0 ) {
+    hasMoreData = false;
+    loader.innerText = 'No more content to load.'
+    return;
+  }
+
+  renderBooks(data.items);
+  startIndex++;
+  isFetching = false;
+  loader.style.display = 'none';
 }
 
 function renderBooks(booksArray) {
@@ -30,21 +43,29 @@ function renderBooks(booksArray) {
     item = item['volumeInfo'];
     book = {
       title: item['title'] || null,
-      author: item['authors'][0] || null,
+      author: item['authors'] ? item['authors'][0] : null,
       pageCount: item['pageCount'] || null,
-      isbn: item['industryIdentifiers'][0]['identifier'] || null,
-      imageUrl: item['imageLinks']['thumbnail'] || null,
-    }
+      isbn: item['industryIdentifiers'] ? item['industryIdentifiers'][0]['identifier'] : null,
+      imageUrl: item['imageLinks'] ? item['imageLinks']['thumbnail'] : '',
+    };
     booksEl.insertAdjacentHTML(
       'beforeend',
       `
-    <div class="book col-md-6">
+    <div class="book col-md-6 my-3">
       <h4>${book.title}</h4>
       <div>Author: <strong>${book.author}</strong></div>
       <div>Pages: <strong>${book.pageCount}</strong></div>
       <div>ISBN: <strong>${book.isbn}</strong></div>
       <img src="${book.imageUrl}" alt="">
     </div>`
-    )
+    );
   });
 }
+
+window.addEventListener('scroll', () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 200 && !isFetching
+  ) {
+    fetchBooks();
+  }
+})
